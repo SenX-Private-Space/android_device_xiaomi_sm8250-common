6f727036c1c44f88018856c96930ad5a1e4ffc9b
kenway214 <kenway214@outlook.com>
2025-03-13 02:45:36 +0530

    sm8250-common: Introduce GameBar v2.0
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>
    Signed-off-by: Rik <aritrarik536@protonmail.com>

--- kona.mk
+++ kona.mk
@@ -374,6 +374,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.power.rc \
     init.qcom.rc \
+    init.munch.rc \
     init.recovery.qcom.rc \
     init.target.rc \
     init.xiaomi.rc \
